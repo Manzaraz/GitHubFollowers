ca5862bd72c4a19d352feb4c31f4a0a85fee06f5
Christian Manzaraz <magnusmanz@gmail.com>
2024-09-06 12:17:31 -0300

    Downloading & Caching Avatar Images

--- GitHubFollowers/Managers/NetworkManager.swift
+++ GitHubFollowers/Managers/NetworkManager.swift
@@ -5,12 +5,13 @@
 //  Created by Christian Manzaraz on 05/09/2024.
 //
 
-import Foundation
+import UIKit
 
 class NetworkManager {
     
     static let shared   = NetworkManager()
-    let baseURL         = "https://api.github.com"
+    private let baseURL         = "https://api.github.com"
+    let cache           = NSCache<NSString, UIImage>()
     
     private init () {}
     
