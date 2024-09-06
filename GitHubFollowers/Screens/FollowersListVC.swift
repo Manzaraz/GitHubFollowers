8b9cc79fcb47ee17a47dfa42013dc462c146d6f5
Christian Manzaraz <magnusmanz@gmail.com>
2024-09-05 23:08:24 -0300

    UICollectionView - FlowLayout

--- GitHubFollowers/Screens/FollowersListVC.swift
+++ GitHubFollowers/Screens/FollowersListVC.swift
@@ -34,7 +34,7 @@ class FollowersListVC: UIViewController {
     func configureCollectionView() {
         collectionView = UICollectionView(
             frame: view.bounds,
-            collectionViewLayout: UICollectionViewLayout()
+            collectionViewLayout: createThreeColumnFlowLayout()
         )
         view.addSubview(collectionView)
         
@@ -44,6 +44,22 @@ class FollowersListVC: UIViewController {
             forCellWithReuseIdentifier: FollowerCell.reuseID
         )
     }
+    
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width                       = view.bounds.width
+        let padding: CGFloat            = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth              = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth                   = availableWidth / 3
+        
+        let flowLayout                  = UICollectionViewFlowLayout()
+        flowLayout.sectionInset         = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize             = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+    
     
     func getFollowers() {
         NetworkManager.shared.getFollowers(for: username, page: 1) { (result) in
