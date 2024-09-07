0b709457aa278b94ba24b9552553fcd31f3f2a8a
Christian Manzaraz <magnusmanz@gmail.com>
2024-09-06 22:11:07 -0300

    Child View Controller - UserInfoHeaderVC

---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Christian Manzaraz on 06/09/2024.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
